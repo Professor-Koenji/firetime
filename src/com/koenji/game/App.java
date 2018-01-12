66d99cbbaa868daa0cf704acc5bfd8d9abf7b895
Brad Davies <varbrad@users.noreply.github.com>
2018-01-12 22:50:39 +0000

    Initial setup of Java environment

---
+++ src/com/koenji/game/App.java
@@ -0,0 +1,27 @@
+package com.koenji.game;
+
+import processing.core.PApplet;
+
+public class App extends PApplet {
+
+  @Override
+  public void settings() {
+    super.settings();
+
+    this.size(1024, 768);
+  }
+
+  @Override
+  public void setup() {
+    super.setup();
+
+    this.surface.setTitle("Firetime");
+
+    this.background(0);
+  }
+
+  @Override
+  public void draw() {
+    super.draw();
+  }
+}
---
+++ src/com/koenji/game/Main.java
@@ -0,0 +1,9 @@
+package com.koenji.game;
+
+import processing.core.PApplet;
+
+public class Main {
+  public static void main(String[] args) {
+    PApplet.main(App.class);
+  }
+}
