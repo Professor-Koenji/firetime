15927b9b91b9483d0b4b38b0d65c4dbcbc60414c
Brad Davies <varbrad@users.noreply.github.com>
2018-03-12 22:13:29 +0000

    Grid-based example

--- src/com/koenji/firetime/Game.java
+++ src/com/koenji/firetime/Game.java
@@ -31,32 +31,45 @@ public class Game extends Core {
 
   public Game() {
     // Set initial resolution, fps & title
-    super(800, 600, 60, "Firetime", 0x30000000);
+    super(800, 600, 60, "Firetime", 0xFF000000);
   }
 
   @Override
   public void init() {
     nodes = new ArrayList<>();
-    randomSeed(millis());
-
-    for (int i = 0; i < 50; ++i) {
-      INode n = new Node((int) random(0, 800), (int) random(0, 600));
-      nodes.add(n);
-      //
-      for (INode n2 : nodes) {
-        if (random(1,10) > 7) n.addNeighbour(n2);
+    for (int y = 0; y < 18; ++y) {
+      for (int x = 0; x < 24; ++x) {
+        INode n = new Node(32 + x * 32, 32 + y * 32);
+        nodes.add(n);
+        if (y > 0) n.addNeighbour(nodes.get((y - 1) * 24 + x));
+        if (x > 0) n.addNeighbour(nodes.get(y * 24 + x - 1));
       }
     }
 
+    for (int i = 0; i < 150; ++i) {
+      int index = (int) random(0, nodes.size());
+      INode node = nodes.remove(index);
+      node.removeAllNeighbours();
+    }
+
     start = nodes.get(0);
-    target = new Node(100, 100);
-    target.addNeighbour(nodes.get(10));
-    nodes.add(target);
+    target = nodes.get(38);
     Pathfinder pf = new Pathfinder(start);
     path = pf.findPath(target);
 
     addEventHandler(InputEvents.MOUSE_MOVED, event -> {
-      start.set(event.position());
+      // Find closest node to mouse cursor
+      PVector targetPos = new PVector(target.getX(), target.getY());
+      PVector mousePos = event.position();
+      float minDist = PVector.dist(mousePos, targetPos);
+      for (INode node : nodes) {
+        float dist = PVector.dist(mousePos, new PVector(node.getX(), node.getY()));
+        if (dist < minDist) {
+          target = node;
+          targetPos.set(target.getX(), target.getY());
+          minDist = dist;
+        }
+      }
       path = pf.findPath(target);
     });
   }
@@ -68,8 +81,8 @@ public class Game extends Core {
     for (INode n : nodes) {
       List<INode> ns = n.getNeighbours();
       for (INode n2 : ns) {
-        stroke(path != null && path.contains(n) && path.contains(n2) ? 0xFF3333FF : 0x01FFFFFF);
-        strokeWeight(1);
+        stroke(path != null && path.contains(n) && path.contains(n2) ? 0xFF3333FF : 0x10FFFFFF);
+        strokeWeight(3);
         line(n.getX(), n.getY(), n2.getX(), n2.getY());
       }
       fill(path != null && path.contains(n) ? 0x603333FF : 0x30FFFFFF);
@@ -81,8 +94,7 @@ public class Game extends Core {
         fill(0xFFFF6666);
         stroke(0xFFFF6666);
       }
-      arc(n.getX(), n.getY(), 16, 16, 0, (float) Math.PI * 2f);
-//      text("x: " + n.getX() + ", y: " + n.getY(), n.getX() + 16, n.getY() + 16);
+      arc(n.getX(), n.getY(), 12, 12, 0, (float) Math.PI * 2f);
     }
   }
 }
