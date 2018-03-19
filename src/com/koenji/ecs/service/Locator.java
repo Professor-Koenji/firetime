882ff82bd6f677bd6bb4da6c758c45cb92db482b
Brad Davies <varbrad@users.noreply.github.com>
2018-03-19 18:37:26 +0000

    Service locator works
    Kind of

--- src/com/koenji/ecs/service/Locator.java
+++ src/com/koenji/ecs/service/Locator.java
@@ -3,28 +3,36 @@ package com.koenji.ecs.service;
 import java.util.HashMap;
 import java.util.Map;
 
-public enum Locator {
-  INSTANCE;
+public class Locator {
 
-  private Map<Class<?>, Object> services = new HashMap<>();
+  private static Map<Class<?>, Object> services = new HashMap<>();
 
-  public <T> void registerService(Class<T> service, Class<? extends T> instance) {
-    if(!services.containsKey(service)){
-      try {
-        Object in = instance.getConstructor().newInstance();
-        services.put(service, in);
-      } catch (Exception e) {
-        throw new IllegalArgumentException("Unable to create service instance");
-      }
+  public static <T> void register(Class<T> service, Class<? extends T> instance) {
+    Locator.register(service, instance, true);
+  }
+
+  public static <T> void register(Class<T> service, T instance) {
+    Locator.register(service, instance, true);
+  }
+
+  public static <T> void register(Class<T> service, Class<? extends T> instance, boolean override) {
+    if (!override && services.containsKey(service)) return;
+    // OK set the service
+    try {
+      Object i = instance.getConstructor().newInstance();
+      services.put(service, i);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Unable to create service instance!");
     }
   }
 
-  public <T> T getService(Class<T> service) {
+  public static <T> void register(Class<T> service, T instance, boolean override) {
+    if (!override && services.containsKey(service)) return;
+    // OK set the service
+    services.put(service, instance);
+  }
 
-    if(services.containsKey(service)) {
-      return service.cast(services.get(service));
-    } else {
-      return null;
-    }
+  public static <T> T get(Class<T> service) {
+    return service.cast(services.getOrDefault(service, null));
   }
 }
