0eee90eed5aedaf62734f413bb79c2269e1ee540
Brad Davies <varbrad@users.noreply.github.com>
2018-03-19 18:55:26 +0000

    Add audio library

--- src/com/koenji/firetime/Game.java
+++ src/com/koenji/firetime/Game.java
@@ -1,11 +1,30 @@
 package com.koenji.firetime;
 
 import com.koenji.ecs.Core;
+import com.koenji.ecs.event.InputEvents;
+import kuusisto.tinysound.Music;
+import kuusisto.tinysound.Sound;
+import kuusisto.tinysound.TinySound;
+
+import java.io.File;
 
 public class Game extends Core {
 
   public Game() {
     // Set initial resolution, fps & title
     super(800, 600, 60, "Firetime", 0xFF000000);
+  }
+
+  @Override
+  public void init() {
+    super.init();
+    //
+    TinySound.init();
+    File sfxFile = new File("assets/sfx/cannon.wav");
+    Sound sfx = TinySound.loadSound(sfxFile);
+
+    addEventHandler(InputEvents.KEY_PRESSED, e -> {
+      sfx.play();
+    });
   }
 }
