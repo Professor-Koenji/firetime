a4cf311cee037b776cbf52b8a4f7f64628e6ece2
Brad Davies <varbrad@users.noreply.github.com>
2018-05-07 11:48:52 +0100

    Cool menu

--- src/com/koenji/firetime/scenes/GameMenu.java
+++ src/com/koenji/firetime/scenes/GameMenu.java
@@ -35,5 +35,18 @@ public class GameMenu extends Scene {
     gc.textAlign(gc.CENTER, gc.CENTER);
     gc.text("FIRETIME", 0, 0);
     gc.popMatrix();
+
+    // Draw level text
+    gc.pushMatrix();
+    gc.translate(gc.getWidth() / 2, 550);
+    gc.rotate((float) Math.cos(rotation * .2f) / 10f);
+    gc.scale(((float) Math.sin(rotation / 2f) + 1) * .2f + 1);
+    gc.textSize(48);
+    gc.fill(0xFFFFFFFF);
+    gc.textAlign(gc.CENTER, gc.CENTER);
+    gc.text("1: level-00", 0, -70);
+    gc.text("2: level-01", 0, 0);
+    gc.text("3: level-02", 0, 70);
+    gc.popMatrix();
   }
 }
