7bee44430f01a9c7dd241654b3846cc8e9c5d033
Brad Davies <varbrad@users.noreply.github.com>
2018-05-08 10:51:02 +0100

    Can make minimap big (M key)

--- src/com/koenji/firetime/systems/Minimap.java
+++ src/com/koenji/firetime/systems/Minimap.java
@@ -1,26 +1,61 @@
 package com.koenji.firetime.systems;
 
+import com.koenji.ecs.event.IEventBus;
+import com.koenji.ecs.event.ISubscriber;
+import com.koenji.ecs.event.InputEvents;
+import com.koenji.ecs.scene.IScene;
+import com.koenji.ecs.service.Locator;
 import com.koenji.ecs.system.render.BasicRenderer;
 import processing.core.PVector;
+
+import java.awt.event.InputEvent;
 
 public class Minimap extends BasicRenderer {
 
   private PVector pos;
   private PVector size;
 
+  private boolean big;
+  private ISubscriber handler;
+
   public Minimap(PVector pos, PVector size) {
     this.pos = pos;
     this.size = size;
+    this.big = false;
+  }
+
+  @Override
+  public void added(IScene scene) {
+    super.added(scene);
+    handler = Locator.get(IEventBus.class).addEventHandler(InputEvents.KEY_PRESSED, e -> {
+      if (e.keyCode() == 77) {
+        this.big = !this.big;
+      }
+    });
+  }
+
+  @Override
+  public void removed() {
+    super.removed();
+    handler.unsubscribe();
   }
 
   @Override
   public void update(int dt) {
     gc.pushMatrix();
-    gc.clip(pos.x, pos.y, size.x, size.y);
-    gc.translate(pos.x + 50, pos.y + 45);
-    gc.scale(0.055f);
-    render();
-    gc.noClip();
+    if (big) {
+      gc.clip(50, 50, gc.getWidth() - 100, gc.getHeight() - 100);
+      gc.translate(350, 225);
+      gc.scale(0.25f);
+      render();
+      gc.noClip();
+    } else {
+      gc.clip(pos.x, pos.y, size.x, size.y);
+      gc.translate(pos.x + 50, pos.y + 45);
+      gc.scale(0.055f);
+      render();
+      gc.noClip();
+    }
     gc.popMatrix();
   }
 }
